f24c060a5d99c794cd14e1af5a7b42c0238c8f1c
jackdry <jdry1729@gmail.com>
2025-09-11 12:45:04 +0100

    removed git hash from version name

--- pytrade/cli/cli.py
+++ pytrade/cli/cli.py
@@ -2,12 +2,7 @@ import logging
 
 import click
 
-from pytrade.cli.dataset import dataset
-from pytrade.cli.portfolio import portfolio
-from pytrade.cli.signal import signal
-from pytrade.cli.net import net
 from pytrade.cli.arctic import arctic
-from pytrade.cli.facts import facts
 from pytrade.cli.schedule import schedule
 
 logging.basicConfig(
@@ -21,12 +16,7 @@ def main():
     pass
 
 
-main.add_command(signal)
-main.add_command(portfolio)
-main.add_command(dataset)
-main.add_command(net)
 main.add_command(arctic)
-main.add_command(facts)
 main.add_command(schedule)
 
 if __name__ == "__main__":
--- pytrade/consts/deribit.py
+++
@@ -1,2 +0,0 @@
-OPTION_NAME_REGEX = ("(?P<base_currency>[A-Z]*)-(?P<expiration_time>[0-9A-Z]*)"
-                     "-(?P<strike>[0-9]*)-(?P<option_type>(C|P))")
